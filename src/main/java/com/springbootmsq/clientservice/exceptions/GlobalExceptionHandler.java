119d1758ded5ec613d6c8b2d07014ece585f9d00
Christian Alba <christian.alba.73@gmail.com>
2025-04-05 23:07:01 -0400

    Organize imports

--- src/main/java/com/springbootmsq/clientservice/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/springbootmsq/clientservice/exceptions/GlobalExceptionHandler.java
@@ -3,7 +3,6 @@ package com.springbootmsq.clientservice.exceptions;
 import java.time.LocalDateTime;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
